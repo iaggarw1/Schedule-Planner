273c6109e954b327876bbf0ffb8b9cabf44ba7f6
Sage Lee <sageriverlee@gmail.com>
2022-04-04 22:01:45 -0700

    Created Calendar

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,9 +3,13 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
@@ -15,16 +19,21 @@ public class Main extends Application {
 			//BorderPane root = new BorderPane();
 			//Scene scene = new Scene(root,400,400);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Parent root = FXMLLoader.load(getClass().getResource("MewTwoLayout.fxml"));
+			primaryStage.show();
+			SplitPane root = FXMLLoader.load(getClass().getResource("MewTwoLayout.fxml"));
 			Scene scene = new Scene(root);
-			
+			GridPane calendar = (GridPane) root.lookup("#calendar");
 			primaryStage.setTitle("Hello World");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			//Fill Calendar with text..
+			System.out.print(calendar);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
